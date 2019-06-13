45417a654ddf08f7bf19710ec77126fdf2deb01b
maknetaRo <makneta@gmail.com>
2019-06-13 22:38:17 +0200

    created count_bulls functiona, tests passed

--- game2.py
+++ game2.py
@@ -1,5 +1,7 @@
 
 def count_bulls(nums, nums_input):
     bulls = 0
-
+    for i in range(len(nums_input)):
+        if nums_input[i] == nums[i]:
+            bulls += 1
     return bulls
--- game2_test.py
+++ game2_test.py
@@ -4,8 +4,10 @@ import game2
 class CountCowsAndBullsTest(unittest.TestCase):
 
     def test_count_bulls(self):
-        
+
         self.assertIsInstance(game2.count_bulls([1, 2, 3], [2, 5, 3]), int)
+        self.assertEqual(game2.count_bulls([1,2,3], [2,5,3]), 1)
+        self.assertEqual(game2.count_bulls([1, 2, 3, 9, 2, 3], [2, 5, 3, 3, 2, 9 ]), 2)
 
 
 

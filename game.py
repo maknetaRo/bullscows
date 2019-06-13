225465134f087209da7cf9e6dd3d4026eca5177c
maknetaRo <makneta@gmail.com>
2019-06-13 17:45:40 +0200

    created play_game and play_again functions

--- game.py
+++ game.py
@@ -1,3 +1,11 @@
+from random import sample, shuffle
+
+def random_numbers():
+    random_nums = sample(range(0,9), 4)
+    if random_nums[0] == 0:
+        shuffle(random_nums)
+    return random_nums
+
 
 def count_cows(nums, nums_input):
     cows = 0
@@ -6,10 +14,37 @@ def count_cows(nums, nums_input):
             cows += 1
     return cows
 
-
 def count_bulls(nums, nums_input):
     bulls = 0
     for i in range(len(nums_input)):
         if nums_input[i] == nums[i]:
             bulls += 1
     return bulls
+
+def play_again():
+    play_again = input("Play again: y/n")
+    while play_again == 'y':
+        play_game()
+
+def play_game():
+    random_nums = random_numbers()
+    score = 0
+    for i in range(7):
+        print("Enter 4 different numbers from 0 to 9.")
+        num1 = int(input("> "))
+        num2 = int(input("> "))
+        num3 = int(input("> "))
+        num4 = int(input("> "))
+        nums_input = [num1, num2, num3, num4]
+        cows = count_cows(random_nums, nums_input)
+        bulls = count_bulls(random_nums, nums_input)
+        print(f"Your numbers are: {num1} {num2} {num3} {num4}.")
+        print(" There are {cows} cows(exist) and {bulls} bulls(match).")
+        if bulls == 4:
+            score += 1
+            print("You won.")
+            break
+
+    play_again()
+
+play_game()
--- game_test.py
+++ game_test.py
@@ -3,6 +3,11 @@ import game
 
 class CountCowsAndBullsTest(unittest.TestCase):
 
+    def test_random_numbers(self):
+        self.assertNotEqual(game.random_numbers(), 0)
+
+        self.assertListEqual(game.random_numbers(), [1, 2, 3, 4])
+
     def test_count_cows(self):
         self.assertEqual(game.count_cows([1, 2, 3, 4], [4, 5, 6, 7]), 1)
         self.assertEqual(game.count_cows([1, 2, 3, 4], [4, 5, 6, 2]), 2)

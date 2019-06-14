5f52188ad6d8f837341ab9ee15307585cf9b3e00
maknetaRo <makneta@gmail.com>
2019-06-15 00:13:23 +0200

    removed random_numbers and random_letters and made random_elements instead

--- game2.py
+++ game2.py
@@ -1,38 +1,49 @@
 from random import randrange, randint, choice, sample
 
-def random_numbers():
+def random_elements():
     number = randint(3, 9)
-    random_numbers = sample(range(1,9), number)
-    return random_numbers
+    kind = randint(0,1)
+    if kind == 0:
+        random_elements = [randint(1, 10) for num in range(3,10)]
+    else:
+        alphabet = [char for char in 'abcdefghijklmnopqrstuvwxyz']
+        random_elements = [choice(alphabet) for elem in range(0, number)]
+    return random_elements
 
-def random_letters():
-    alphabet = [char for char in 'abcdefghijklmnopqrstuvwxyz']
-    number = randint(3, 9)
-    random_letters = sample(alphabet, number)
-    return random_letters
-
-def count_bulls(nums, nums_input):
+def count_bulls(elements, elements_input):
     bulls = 0
-    for i in range(len(nums_input)):
-        if nums_input[i] == nums[i]:
+    for i in range(len(elements_input)):
+        if elements_input[i] == elements[i]:
             bulls += 1
     return bulls
 
-def count_cows(nums, nums_input):
+def count_cows(elements, elements_input):
     cows = 0
-    for i in range(len(nums_input)):
-        if (nums_input[i] != nums[i]) and (nums_input[i] in nums):
+    for i in range(len(elements_input)):
+        if (elements_input[i] != elements[i]) and (elements_input[i] in elements):
             cows += 1
     return cows
 
-def play_game():
-    kind = randint(0,1)
-    if kind == 0:
-        secret = random_numbers()
-    else:
-        secret = random_letters()
-    return secret
 
-    # bulls = 0
-    # cows =  0
-    # print("Bulls: {}, cows: {}.".format(bulls, cows))
+def get_input(random_elements):
+    secret_guesses = []
+    if all(isinstance(n, int) for n in random_elements):
+        print("Enter {} numbers from 1 to 9 (one digit per input)".format(len(random_elements)))
+        for elem in range(len(random_elements)):
+            elem = int(input(">"))
+            secret_guesses.append(elem)
+    else:
+        print("Enter {} letters (one letter per input)".format(len(random_elements)))
+        for elem in range(len(random_elements)):
+            elem = input(">").lower()
+            secret_guesses.append(elem)
+    return secret_guesses
+
+def play_game(secret_guesses):
+
+    for elem in range(7):
+        get_input(secret_guesses)
+        cows = count_cows(random_elements, secret_guesses)
+        bulls = count_bulls(random_elements, secret_guesses)
+        print("Your elements are: {}".format(secret_guesses))
+        print("Bulls: {}, cows: {}.".format(bulls, cows))
--- game2_test.py
+++ game2_test.py
@@ -4,7 +4,6 @@ import game2
 class CountCowsAndBullsTest(unittest.TestCase):
 
     def test_count_bulls(self):
-
         self.assertIsInstance(game2.count_bulls([1, 2, 3], [2, 5, 3]), int)
         self.assertEqual(game2.count_bulls([1,2,3], [2,5,3]), 1)
         self.assertEqual(game2.count_bulls([1, 2, 3, 9, 2, 3], [2, 5, 3, 3, 2, 9 ]), 2)
@@ -13,18 +12,18 @@ class CountCowsAndBullsTest(unittest.TestCase):
         self.assertIsInstance(game2.count_cows([1,2,3], [2,5,3]), int)
         self.assertEqual(game2.count_cows([1,2,3], [2,5,3]), 1)
         self.assertEqual(game2.count_cows([1, 2, 3, 9, 2, 3], [2, 5, 3, 3, 2, 9 ]), 3)
+    #
+    # def test_random_elements(self):
+    #     self.assertIsInstance(game2.random_elements(random_elements), list)
 
-    def test_random_numbers(self):
-        self.assertIsInstance(game2.random_numbers(), list)
-
-    def test_random_letters(self):
-        self.assertIsInstance(game2.random_letters(), list)
+    # def test_get_input(self):
+    #     #self.assertEqual(game2.get_input(), "Enter X numbers from 1 to 9.")
+    #     self.assertIsInstance(game2.get_input(), list)
 
     def test_play_game(self):
-        self.assertTrue(game2.play_game())
-        self.assertIsInstance(game2.play_game(), list)
-    #     self.assertEqual(game2.play_game(), "Bulls: 0, cows: 0.")
-    #     self.assertEqual(game2.play_game([1,2,3], [4,5,6]), "Bulls: 0, cows: 0.")
+        guess_secret = [1, 2, 3]
+        self.assertEqual(game2.play_game(), "Bulls: 0, cows: 0.")
+        self.assertEqual(game2.play_game(), "Bulls: 0, cows: 0.")
 
 
 

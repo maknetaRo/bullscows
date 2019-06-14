7920867b2c0973da22d5e7b031d467edff271002
maknetaRo <makneta@gmail.com>
2019-06-14 18:26:32 +0200

    added random_numbers, random_letters and play_game functions with some tests

--- game2.py
+++ game2.py
@@ -1,3 +1,15 @@
+from random import randrange, randint, choice, sample
+
+def random_numbers():
+    number = randint(3, 9)
+    random_numbers = sample(range(1,9), number)
+    return random_numbers
+
+def random_letters():
+    alphabet = [char for char in 'abcdefghijklmnopqrstuvwxyz']
+    number = randint(3, 9)
+    random_letters = sample(alphabet, number)
+    return random_letters
 
 def count_bulls(nums, nums_input):
     bulls = 0
@@ -10,5 +22,17 @@ def count_cows(nums, nums_input):
     cows = 0
     for i in range(len(nums_input)):
         if (nums_input[i] != nums[i]) and (nums_input[i] in nums):
-            cows += 1  
+            cows += 1
     return cows
+
+def play_game():
+    kind = randint(0,1)
+    if kind == 0:
+        secret = random_numbers()
+    else:
+        secret = random_letters()
+    return secret
+
+    # bulls = 0
+    # cows =  0
+    # print("Bulls: {}, cows: {}.".format(bulls, cows))
--- game2_test.py
+++ game2_test.py
@@ -14,6 +14,18 @@ class CountCowsAndBullsTest(unittest.TestCase):
         self.assertEqual(game2.count_cows([1,2,3], [2,5,3]), 1)
         self.assertEqual(game2.count_cows([1, 2, 3, 9, 2, 3], [2, 5, 3, 3, 2, 9 ]), 3)
 
+    def test_random_numbers(self):
+        self.assertIsInstance(game2.random_numbers(), list)
+
+    def test_random_letters(self):
+        self.assertIsInstance(game2.random_letters(), list)
+
+    def test_play_game(self):
+        self.assertTrue(game2.play_game())
+        self.assertIsInstance(game2.play_game(), list)
+    #     self.assertEqual(game2.play_game(), "Bulls: 0, cows: 0.")
+    #     self.assertEqual(game2.play_game([1,2,3], [4,5,6]), "Bulls: 0, cows: 0.")
+
 
 
 if __name__ == '__main__':
